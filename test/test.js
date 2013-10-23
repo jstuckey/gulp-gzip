785f95e8d5ab4ec82aadc2fe737904ec939d393a
jstuckey <klash64@gmail.com>
2013-10-22 21:15:07 -0400

    Added new test.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var gzip = require("../");
+var zlib = require("zlib");
 var should = require("should");
 var gulp = require("gulp");
 var es = require("event-stream");
@@ -31,7 +32,7 @@ describe("gulp-gzip", function() {
 			}));
 		});
 
-		it("should create a file in the destination", function(done) {
+		it("should create a .gz file", function(done) {
 
 			var outStream = gulp.dest("./")
 
@@ -39,13 +40,44 @@ describe("gulp-gzip", function() {
 				// The new file was written to the file system
 				fs.readFile("./input.txt.gz", function(err, file) {
 
-					console.log(util.inspect(file));
+					// console.log(util.inspect(file));
 
+					// Check if the file was found
 					should.not.exist(err);
   					should.exist(file);
 					file.should.not.be.empty;
 
 					done();
+				});
+			});
+
+			gulp.src("*.txt")
+			.pipe(gzip())
+			.pipe(outStream);
+		});
+
+		it("should match uncompressed file with original file", function(done) {
+
+			var outStream = gulp.dest("./")
+
+			outStream.on("close", function() {
+
+				// Get compressed file
+				fs.readFile("./input.txt.gz", function(err, file) {
+					// Uncompress the file
+					zlib.unzip(file, function(err, uncompressedFileBuffer) {
+
+						uncompressedFile = uncompressedFileBuffer.toString("utf8", 0, uncompressedFileBuffer.length);
+						// console.log(util.inspect(uncompressedFile));
+
+						// Get original file
+						fs.readFile("./input.txt", {encoding: "utf8"}, function(err, originalFile) {
+							// console.log(util.inspect(originalFile));
+							originalFile.should.equal(uncompressedFile);
+
+							done();
+						});
+					});
 				});
 			});
 
