d79ba6ad367398c065afdc9d5a652e39181420f2
jstuckey <klash64@gmail.com>
2013-10-22 21:19:43 -0400

    Added comments to test file.

--- test/test.js
+++ test/test.js
@@ -15,6 +15,7 @@ describe("gulp-gzip", function() {
 			gulp.src("*.txt")
 			.pipe(gzip())
 			.pipe(es.map(function(file, cb) {
+				// Check if file properties end with .gz
 				file.path.should.endWith("gz");
 				file.shortened.should.endWith("gz");
 				cb(null, null);
@@ -26,6 +27,7 @@ describe("gulp-gzip", function() {
 			gulp.src("*.txt")
 			.pipe(gzip())
 			.pipe(es.map(function(file, cb) {
+				// Check if file contents is a Buffer object
 				file.contents.should.be.instanceof(Buffer); // should.have.type didn't work
 				cb(null, null);
 				done();
@@ -36,8 +38,9 @@ describe("gulp-gzip", function() {
 
 			var outStream = gulp.dest("./")
 
+			// Capture close event of the write stream so we know when gulp.dest finishes
 			outStream.on("close", function() {
-				// The new file was written to the file system
+				// Get the compressed file
 				fs.readFile("./input.txt.gz", function(err, file) {
 
 					// console.log(util.inspect(file));
@@ -60,19 +63,22 @@ describe("gulp-gzip", function() {
 
 			var outStream = gulp.dest("./")
 
+			// Capture close event of the write stream so we know when gulp.dest finishes
 			outStream.on("close", function() {
 
-				// Get compressed file
+				// Get the compressed file
 				fs.readFile("./input.txt.gz", function(err, file) {
 					// Uncompress the file
 					zlib.unzip(file, function(err, uncompressedFileBuffer) {
-
+						// Convert buffer to utf8 string
 						uncompressedFile = uncompressedFileBuffer.toString("utf8", 0, uncompressedFileBuffer.length);
 						// console.log(util.inspect(uncompressedFile));
 
-						// Get original file
+						// Get original file as utf8 string
 						fs.readFile("./input.txt", {encoding: "utf8"}, function(err, originalFile) {
 							// console.log(util.inspect(originalFile));
+
+							// Compare the original file to the uncompressed .gz file
 							originalFile.should.equal(uncompressedFile);
 
 							done();
