03bb659002e676299eb584b3cd92bffe908b8f42
jstuckey <klash64@gmail.com>
2013-10-24 09:59:03 -0400

    Removed util.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var es = require("event-stream");
 var zlib = require("zlib");
 var clone = require('clone');
-util = require("util");
 
 module.exports = function() {
 	'use strict';
@@ -12,8 +11,6 @@ module.exports = function() {
 		var newFile = clone(file);
 		newFile.path += '.gz';
 		newFile.shortened += '.gz';
-
-		// console.log(util.inspect(newFile));
 
 		// Use zlib to compress the file
 		zlib.gzip(newFile.contents, function(err, buffer) {
