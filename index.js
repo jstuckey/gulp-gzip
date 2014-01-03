e306e84ac20f8f8bc645fcceb26a74b52046836d
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-01-03 20:39:54 +0100

    Adding buffer support

--- index.js
+++ index.js
@@ -7,21 +7,28 @@ module.exports = function() {
 
 	function compress(file, callback) {
 
-		// Clone file and append .gz file extension
-		var newFile = clone(file);
-		newFile.path += '.gz';
-		newFile.shortened += '.gz';
-
 		// Use zlib to compress the file
-		zlib.gzip(newFile.contents, function(err, buffer) {
-			if (!err) {
-				// Set the compressed file contents
-		    	newFile.contents = buffer;
-		    	callback(null, newFile);
-		  	} else {
-		  		callback(err, null);
-		  	}
-		});
+		if(file.isBuffer()) {
+		
+	    // Clone file and append .gz file extension
+	    var newFile = clone(file);
+	    newFile.path += '.gz';
+	    newFile.shortened += '.gz';
+		  zlib.gzip(newFile.contents, function(err, buffer) {
+			  if (!err) {
+				  // Set the compressed file contents
+		      	newFile.contents = buffer;
+		      	callback(null, newFile);
+		    	} else {
+		    		callback(err, null);
+		    	}
+		  });
+		} else {
+	    file.path += '.gz';
+	    file.shortened += '.gz';
+		  file.contents = file.contents.pipe(zlib.createGzip());
+     	callback(null, file);
+		}
 	}
 
 	return es.map(compress);
