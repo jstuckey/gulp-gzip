4123c742fcc676f76e605cef05e70d73d58c3858
jstuckey <klash64@gmail.com>
2014-01-06 20:30:40 -0500

    Fixed and cleaned up stream support code. New code for latest gulp version.

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 var es = require("event-stream");
 var zlib = require("zlib");
-var clone = require('clone');
+var clone = require("clone");
+var through = require("through");
 
 module.exports = function() {
 	'use strict';
 
 	function compress(file, callback) {
 
-		// Use zlib to compress the file
+		 if (file.isNull()) return callback(null, file); // pass along
+
+		// Clone file and append .gz file extension
+		var newFile = clone(file);
+		newFile.path += '.gz';
+		newFile.shortened += '.gz';
+
+		// Check if file is a buffer or a stream
 		if(file.isBuffer()) {
-		
-	    // Clone file and append .gz file extension
-	    var newFile = clone(file);
-	    newFile.path += '.gz';
-	    newFile.shortened += '.gz';
-		  zlib.gzip(newFile.contents, function(err, buffer) {
-			  if (!err) {
-				  // Set the compressed file contents
-		      	newFile.contents = buffer;
-		      	callback(null, newFile);
-		    	} else {
-		    		callback(err, null);
-		    	}
-		  });
+
+			// File contents is a buffer
+			zlib.gzip(newFile.contents, function(err, buffer) {
+				if (!err) {
+					// Set the compressed file contents
+					newFile.contents = buffer;
+					callback(null, newFile);
+				} else {
+					callback(err, null);
+				}
+			});
 		} else {
-	    file.path += '.gz';
-	    file.shortened += '.gz';
-		  file.contents = file.contents.pipe(zlib.createGzip());
-     	callback(null, file);
+
+			// File contents is a stream
+			var gzipStream = zlib.createGzip();
+			var throughStream = through();
+			newFile.contents = file.contents.pipe(gzipStream).pipe(throughStream);
+			callback(null, newFile);
 		}
 	}
 
