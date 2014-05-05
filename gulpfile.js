aafb0b66a3b91f3a0b7d9a56f700b2103bbae011
Aleksej Kuchmych <aleksej.kuchmych@gmail.com>
2014-05-05 15:03:21 +0300

    Updated gulp workflow: - package.json updates on gulp(3.6.0 => 3.6.2) and gulp-watch(0.5.3 => 0.6.4) - now using new features of gulp-watch: instead of streaming fetish, now we able to run gulp tasks as an <Array>

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,6 @@ var stylish = require('jshint-stylish');
 
 var root = __dirname;
 
-gulp.task('watch', function() {
-  gulp.src([ 'index.js', 'lib/*.js' , 'test/test.js' ], { read: false,  })
-    .pipe(watch({ /*emit: 'all',*/ name: 'Mocha', read: false }, function(files) {
-      // monkeys are fixing `cwd` for `gulp-mocha`
-      // node lives in one process/scope/directory
-      process.chdir(root);
-
-      gulp.src('test/test.js')
-        .pipe(mocha({ reporter: 'spec', timeout: 1000 }));
-    }));
-});
-
 gulp.task('clean', function() {
   gulp.src([
     'examples/*/tmp',
@@ -31,6 +19,23 @@ gulp.task('lint', function() {
   gulp.src([ 'index.js', 'test/test.js', 'lib/*.js' ])
     .pipe(jshint({ expr: true }))
     .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('test', function() {
+  // monkeys are fixing `cwd` for `gulp-mocha`
+  // node lives in one process/scope/directory
+  process.chdir(root);
+
+  gulp.src('test/test.js')
+    .pipe(mocha({ reporter: 'spec', timeout: 1000 }))
+});
+
+gulp.task('watch', function() {
+  watch({
+    emit: 'all',
+    glob: [ 'index.js', 'lib/*.js' , 'test/test.js' ],
+    read: false
+  }, ['clean', 'lint', 'test'])
 });
 
 gulp.task('default', ['watch']);
