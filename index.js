9c8281009fbd9f8e9678ca3a1e1955f3b816ed71
Neil Carpenter <neil.carpenter@unit9.com>
2014-12-12 00:15:50 +0000

    add svg to gzippable filetypes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var utils       = require('./lib/utils');
 var bufferMode  = require('./lib/bufferMode');
 var streamMode  = require('./lib/streamMode');
 
-var gzipabble = ['.css', '.js', '.txt', '.json', '.eot', '.ttf', '.xml'];
+var gzipabble = ['.css', '.js', '.txt', '.json', '.eot', '.ttf', '.xml', '.svg'];
 
 var PLUGIN_NAME = 'gulp-gzip';
 
