1c8936256cb529382580a39abdec1d5f984d1a7a
jstuckey <klash64@gmail.com>
2015-03-29 09:56:07 -0400

    Changed tabs to spaces.
    I used to prefer tabs for whatever reason, but it's started to get on my
    nerves. Tabs look especially bad when viewing the file on GitHub.

--- index.js
+++ index.js
@@ -11,60 +11,60 @@ var PLUGIN_NAME = 'gulp-gzip';
 
 module.exports = function (options) {
 
-	// Combine user defined options with default options
-	var config = utils.merge({ append: true, threshold: false, gzipOptions: {} }, options);
+  // Combine user defined options with default options
+  var config = utils.merge({ append: true, threshold: false, gzipOptions: {} }, options);
 
-	// Create a through2 object stream. This is our plugin export
-	var stream = through2.obj(compress);
+  // Create a through2 object stream. This is our plugin export
+  var stream = through2.obj(compress);
 
-	// Expose the config so we can test it
-	stream.config = config;
+  // Expose the config so we can test it
+  stream.config = config;
 
-	function compress(file, enc, done) {
+  function compress(file, enc, done) {
 
-		/*jshint validthis: true */
-		var self = this;
+    /*jshint validthis: true */
+    var self = this;
 
-		// Check for empty file
-		if (file.isNull()) {
-			// Pass along the empty file to the next plugin
-			self.push(file);
-			done();
-			return;
-		}
+    // Check for empty file
+    if (file.isNull()) {
+      // Pass along the empty file to the next plugin
+      self.push(file);
+      done();
+      return;
+    }
 
-		// Call when finished with compression
-		var finished = function(err, contents, wasCompressed) {
-			if (err) {
-				var error = new PluginError(PLUGIN_NAME, err, { showStack: true });
-				self.emit('error', error);
-				done();
-				return;
-			}
+    // Call when finished with compression
+    var finished = function(err, contents, wasCompressed) {
+      if (err) {
+        var error = new PluginError(PLUGIN_NAME, err, { showStack: true });
+        self.emit('error', error);
+        done();
+        return;
+      }
 
-			if (wasCompressed) {
-				if (file.contentEncoding) {
-					file.contentEncoding.push('gzip');
-				} else {
-					file.contentEncoding = [ 'gzip' ];
-				}
-				if (config.append) {
-					file.path += '.gz';
-				}
-			}
-			file.contents = contents;
-			self.push(file);
-			done();
-			return;
-		};
+      if (wasCompressed) {
+        if (file.contentEncoding) {
+          file.contentEncoding.push('gzip');
+        } else {
+          file.contentEncoding = [ 'gzip' ];
+        }
+        if (config.append) {
+          file.path += '.gz';
+        }
+      }
+      file.contents = contents;
+      self.push(file);
+      done();
+      return;
+    };
 
-		// Check if file contents is a buffer or a stream
-		if (file.isBuffer()) {
-			bufferMode(file.contents, config, finished);
-		} else {
-			streamMode(file.contents, config, finished);
-		}
-	}
+    // Check if file contents is a buffer or a stream
+    if (file.isBuffer()) {
+      bufferMode(file.contents, config, finished);
+    } else {
+      streamMode(file.contents, config, finished);
+    }
+  }
 
-	return stream;
+  return stream;
 };
